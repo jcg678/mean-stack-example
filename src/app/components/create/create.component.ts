3bdd24b278ff8ce88d41163b435a94722b74df01
jcg678 <jcg678@hotmail.es>
2019-10-16 20:39:17 +0200

    subiendo imagen

--- src/app/components/create/create.component.ts
+++ src/app/components/create/create.component.ts
@@ -2,22 +2,25 @@ import { Component, OnInit } from '@angular/core';
 import {Project} from '../../models/project';
 
 import {ProjectService} from '../../services/project.service';
-
+import { UploadService } from '../../services/upload.service';
+import { Global } from '../../services/global';
 
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
   styleUrls: ['./create.component.css'],
-  providers: [ProjectService]
+  providers: [ProjectService, UploadService]
 })
 export class CreateComponent implements OnInit {
 
   public title:string;
   public project: Project;
   public status:string;
+  public filesToUpload: Array<File>;
 
   constructor(
-  	private _projectService: ProjectService
+  	private _projectService: ProjectService,
+  	private _uploadService: UploadService
 
   	){ 
   	this.title = "Crear Proyecto";
@@ -33,8 +36,12 @@ export class CreateComponent implements OnInit {
   	this._projectService.saveProject(this.project).subscribe(
   		response=>{
   			if(response.project){
-  				this.status = 'success';
-  				form.reset();
+  				
+  				this._uploadService.makeFileRequest(Global.url+"upload-image/"+response.project._id, [], this.filesToUpload, 'image').then((result:any)=>{
+  					this.status = 'success';
+  					form.reset();
+  				});
+  				
   			}else{
   				this.status = 'failed';
   			}
@@ -43,6 +50,10 @@ export class CreateComponent implements OnInit {
   			console.log(<any>error);
   		}
   	);
+  }
+
+   fileChangeEvent(fileInput: any){
+  	this.filesToUpload = <Array<File>>fileInput.target.files;
   }
 
 }
